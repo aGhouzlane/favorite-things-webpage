6b001b9fdb4e659e32ff28da1200fff7b94c0fc0
aGhouzlane <ahmedghouzlane@gmail.com>
2021-03-29 15:00:41 -0400

    Add an array an display it in ul
    Co-authored-by: Brandon Magofna <Magofna68@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,15 @@ $(document).ready(function () {
 
     var inputs = [fInput, sInput, tInput];
 
-    console.log(inputs);
+    var newArray = [];
+    newArray.push(inputs[0]);
+    newArray.push(inputs[1]);
+    newArray.push(inputs[2]);
+
+
+    $("ul").append("<li>" + newArray[0] + "</li>");
+    $("ul").append("<li>" + newArray[1] + "</li>");
+    $("ul").append("<li>" + newArray[2] + "</li>");
 
   });
 
